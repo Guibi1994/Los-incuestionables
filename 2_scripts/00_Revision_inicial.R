06def11251b250dd802d534a1210902a84f0e116
Guibor Camargo <guibor.camargo@urosario.edu.co>
2022-07-20 22:36:02 -0500

    20jul

--- 2_scripts/00_Revision_inicial.R
+++ 2_scripts/00_Revision_inicial.R
@@ -4,13 +4,23 @@ library(stringr)
 library(tidyr)
 library(ggplot2)
 
-a0_raw_congreso <- read.csv("1_datos/01_congreso_base_funcion_publica.csv")
 
+b0_raw_congreso <- read.csv("1_datos/01_congreso_base_funcion_publica.csv")
+
+
+79940745 # Duqe
+79289575 # Roy Barreras
+
+View(b0_raw_congreso %>% filter(NUMERO_DOCUMENTO == "79289575"))
 # Registros por cédula
-pr <- a0_raw_congreso %>% 
-  group_by(NUMERO_DOCUMENTO) %>% 
+b1_resumen <- b0_raw_congreso %>%
+  select(NUMERO_DOCUMENTO,CARGO_DECLARANTE,
+         PRIMER_NOMBRE_DECLARANTE_PN, SEGUCLARANTE_PN, SEGUNDO_APELLIDO_DECLARANTE_PN,
+         ANO_GRAVABLE_DECLARAC_IYR, DECLARA_RENTA_DIAN, NUMERO_FORMULARIO, TOTAL_INGRESOS_PERS_NATURAL, DECLARACION_RENTA_DIAN) %>% 
+  group_by(NUMERO_DOCUMENTO) %>%
+  mutate(registros = n()) %>% 
+  arrange(-registros)#
   summarise(registros = n(),
             min_year = min(ANO_GRAVABLE_DECLARAC_IYR, na.rm = T),
-            max_year = max(ANO_GRAVABLE_DECLARAC_IYR, na.rm = T),
-            pr = as.list(ANO_GRAVABLE_DECLARAC_IYR)) %>% 
+            max_year = max(ANO_GRAVABLE_DECLARAC_IYR, na.rm = T)) %>% 
   as.data.frame()
--- 2_scripts/02_DIA_pdf_lectura.R
+++ 2_scripts/02_DIA_pdf_lectura.R
@@ -1,10 +1,37 @@
 library(pdftools)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(ggplot2)
+options(scipen = 100)
+
+
+a0_DIAN <- pdf_text("1_datos/pureba_DIAN.pdf") %>% 
+  str_split(., "\\s{4,}", simplify = T) %>% 
+  as.data.frame()
 
 
 
-pr <- pdf_text("1_datos/pureba_DIAN.pdf") %>% 
-  str_split(., "\n")
 
-str_view(pr[[1]][3], "[^\\D\\s]+\\D")
+a1_reporte <- data.frame(
+  # Cedula o ID de la persona
+  id_persona = as.numeric(str_remove_all(a0_DIAN$V4, "\\s")),
+  # ID del formulario DIAN
+  id_formulario_DIAN = as.numeric(a0_DIAN$V3),
+  # Año de la declaración
+  year = as.integer(str_sub(a0_DIAN$V133, 1,4)),
+  # Rubros tributarios
+  total_patrimonio_bruto = as.numeric(str_remove_all(a0_DIAN$V15, "\\,")),
+  total_deudas = as.numeric(str_remove_all(a0_DIAN$V15, "\\,")),
+  total_patrimonio_liquido = as.numeric(str_remove_all(a0_DIAN$V16, "\\,")),
+  ganancias_ocacionales = as.numeric(str_remove_all(a0_DIAN$V86, "\\,")))
+
+
+
+c1_id <- str_replace_all(a0_DIAN[[1]][3],"\\s*","")
+
+
+
+str_view(c1_id, "\\d+")
 
 
